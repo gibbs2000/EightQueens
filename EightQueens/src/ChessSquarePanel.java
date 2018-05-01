80f1d5af5a6613c596f95a4e138c2e4476fb88bc
Sean Gibbons <gibbonss@bishopireton.org>
2018-05-01 14:09:42 -0400

    updated

--- EightQueens/src/ChessSquarePanel.java
+++ EightQueens/src/ChessSquarePanel.java
@@ -2,9 +2,6 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
-import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 
 /**
@@ -18,8 +15,6 @@ public class ChessSquarePanel extends JPanel {
 	// These fields are used whenever PaintComponent is executed to update the panel
 	private boolean isQueen;
 	private Color backColor;
-	private ImageIcon image;
-	private JLayeredPane layers;
 	private static final int FONTSIZE = 30;
 	private static final Font title = new Font("Comic Sans MS", Font.BOLD, FONTSIZE);
 
@@ -29,33 +24,26 @@ public class ChessSquarePanel extends JPanel {
 	}
 
 	ChessSquarePanel(Color c, boolean q) {
-		layers = new JLayeredPane();
 		backColor = c;
 		isQueen = q;
-	}
-
-	ChessSquarePanel(String s, boolean q) {
-		this.setLayout(null);
-		layers = new JLayeredPane();
-		layers.setBounds(this.getBounds());
-		image = new ImageIcon(s);
-		JLabel label = new JLabel("", image, JLabel.CENTER);
-		layers.add(label, new Integer(1));
-		this.add(layers);
-
 	}
 
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		this.setBackground(backColor);
-		if (isQueen) {
-			JLabel label = new JLabel("Q");
-			label.setFont(title);
-			label.setBackground(Color.RED);
-			layers.add(label, new Integer(2));
-			this.add(layers);
 
+		this.setBackground(backColor);
+
+		// removeAll(); // Someone mentioned panels not updating properly, this
+		// may help, but does not seem necessary
+		// x and y center the String, adjust as necessary
+
+		if (isQueen) {
+			g.setFont(title);
+			g.setColor(Color.RED);
+			int x = (this.getWidth() / 2) - FONTSIZE / 4; // - letter.length()/2;
+			int y = (this.getHeight() / 2) + FONTSIZE / 4;
+			g.drawString("Q", x, y);
 		}
 	}
 
--- EightQueens/src/EightQueens.java
+++ EightQueens/src/EightQueens.java
@@ -6,6 +6,7 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Stack;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -23,20 +24,22 @@ public class EightQueens {
 	JFrame window;
 	JPanel header, footer, grid;
 	ChessSquarePanel[][] squares;
-	ArrayList<Queen> queens;
+	Stack<Queen> queens;
+	boolean isRunning;
 
 	// These are constant fields for numerical values and colors
-	final int HEIGHT = 1000, WIDTH = 1000, ROWS = 8, COLUMNS = 8, GRID_SIZE = 800;
+	public static final int HEIGHT = 1000, WIDTH = 1000, ROWS = 8, COLUMNS = 8;
 	private static final Color DEFAULT_BACKGROUND = Color.CYAN;
 	private static final Font title = new Font("Comic Sans MS", Font.BOLD, 26);
 	private static final Font labelText = new Font("Comic Sans MS", Font.PLAIN, 18);
-	private static final String whiteTexture = "wht.jpg";
-	private static final String blackTexture = "blk.jpeg";
+	private static final String COPYRIGHT = "This program is the legal property of Sean Gibbons, with graphics assistance by Mrs Kelly",
+			TITLE = "Eight Queens", EXAMPLE = "Click to show an example", RESET = "Click to reset",
+			RECURSE = "Click to run the recursive method";
 
 	public EightQueens() {
+		queens = new Stack<Queen>();
 
 		createFrame();
-
 		createHeader();
 		createGrid();
 		createFooter();
@@ -49,7 +52,7 @@ public class EightQueens {
 	}
 
 	public void createFrame() {
-		window = new JFrame("Practicing");
+		window = new JFrame(TITLE);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setSize(new Dimension(WIDTH, HEIGHT));
 		window.setLayout(new BoxLayout(window.getContentPane(), BoxLayout.Y_AXIS));
@@ -58,7 +61,7 @@ public class EightQueens {
 	public void createHeader() {
 		header = new JPanel();
 		header.setBackground(DEFAULT_BACKGROUND);
-		JLabel label = new JLabel("EIGHT QUEENS");
+		JLabel label = new JLabel(TITLE);
 		label.setFont(title);
 		header.add(label);
 
@@ -68,20 +71,18 @@ public class EightQueens {
 		squares = new ChessSquarePanel[ROWS][COLUMNS];
 
 		grid = new JPanel();
-		grid.setMinimumSize(new Dimension(WIDTH, GRID_SIZE));
-		grid.setMaximumSize(new Dimension(WIDTH, GRID_SIZE));
-		grid.setPreferredSize(new Dimension(WIDTH, GRID_SIZE));
+		grid.setMinimumSize(new Dimension(WIDTH, 800));
+		grid.setMaximumSize(new Dimension(WIDTH, 800));
+		grid.setPreferredSize(new Dimension(WIDTH, 800));
 		grid.setLayout(new GridLayout(ROWS, COLUMNS));
 		int x = 0;
 		for (int i = 0; i < ROWS; i++) {
 			for (int j = 0; j < COLUMNS; j++) {
-				if (x++ % 2 == 0) {
-					squares[i][j] = new ChessSquarePanel(blackTexture, false);
-					// squares[i][j] = new ChessSquarePanel(Color.DARK_GRAY, false);
-				} else {
-					squares[i][j] = new ChessSquarePanel(whiteTexture, false);
-					// squares[i][j] = new ChessSquarePanel(Color.WHITE, false);
-				}
+				if (x++ % 2 == 0)
+					squares[i][j] = new ChessSquarePanel(Color.GRAY, false);
+
+				else
+					squares[i][j] = new ChessSquarePanel(Color.WHITE, false);
 				grid.add(squares[i][j]);
 			}
 			x++;
@@ -93,7 +94,7 @@ public class EightQueens {
 		footer = new JPanel();
 		footer.setLayout(new BorderLayout(10, 10));
 		footer.setBackground(DEFAULT_BACKGROUND);
-		JLabel label = new JLabel("This program copyright Sean Gibbons, with graphics assistance by Mrs Kelly");
+		JLabel label = new JLabel(COPYRIGHT);
 		label.setFont(labelText);
 		footer.add(label, BorderLayout.BEFORE_FIRST_LINE);
 		JButton showExample = exampleButton();
@@ -109,7 +110,7 @@ public class EightQueens {
 	public JButton exampleButton() {
 		JButton example = new JButton();
 		example.setHorizontalTextPosition(SwingConstants.CENTER);
-		JLabel label = new JLabel("Click to show an example");
+		JLabel label = new JLabel(EXAMPLE);
 		label.setFont(labelText);
 		example.add(label);
 		example.addActionListener(new ActionListener() {
@@ -126,35 +127,33 @@ public class EightQueens {
 		JButton recursion = new JButton();
 		recursion.setHorizontalTextPosition(SwingConstants.CENTER);
 
-		JLabel label = new JLabel("Click to run the recursive method", JLabel.CENTER);
+		JLabel label = new JLabel(RECURSE, JLabel.CENTER);
 		label.setFont(labelText);
 		recursion.add(label);
 		recursion.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// TODO write the recursive method
+				if (!isRunning) {
+					addQueens(queens);
+					updateQueens();
+				}
+
 			}
 		});
 		return recursion;
 	}
 
-	public boolean addQueens(ArrayList<Queen> alreadyPlaced) {
-		if (alreadyPlaced.isEmpty()) {
-
-		}
-		if (alreadyPlaced.size() > 8) {
-			return true;// base case
-		} else {
-
-			return false;
-
+	private void updateQueens() {
+		for (Queen q : queens) {
+			squares[q.getColumn()][q.getRow()].setQueen(true);
+			;
 		}
 	}
 
 	public JButton resetButton() {
 		JButton reset = new JButton();
 		reset.setHorizontalTextPosition(SwingConstants.CENTER);
-		JLabel label = new JLabel("Click to reset");
+		JLabel label = new JLabel(RESET);
 		label.setFont(labelText);
 		reset.add(label);
 		reset.addActionListener(new ActionListener() {
@@ -181,4 +180,31 @@ public class EightQueens {
 			}
 		}
 	}
+
+	public boolean addQueens(Stack<Queen> alreadyPlaced) {
+		isRunning = true;
+		if (alreadyPlaced.isEmpty()) {
+			queens.add(new Queen(0, 0));
+		}
+		if (alreadyPlaced.size() > 8) {
+			isRunning = false;
+			return true;// base case
+		} else {
+			for (int i = alreadyPlaced.peek().getRow(); i < ROWS; i++) {
+				for (int j = alreadyPlaced.peek().getColumn(); j < ROWS; j++) {
+					Queen q = new Queen(i, j);
+					if (isLegal(q, alreadyPlaced)) {
+						alreadyPlaced.add(q);
+					}
+				}
+			}
+		}
+		return isRunning;
+	}
+
+	public boolean isLegal(Queen q, Stack<Queen> alreadyPlaced) {
+
+		return true;
+	}
+
 }
